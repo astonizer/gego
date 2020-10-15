98c06872fe0ba8cf9261de862c830228de7f53fe
Astel <astelthottankara@gmail.com>
2020-10-15 16:34:30 +0530

    Add internet check

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
 
 const axios = require('axios')
+const checkInternetConnected = require('check-internet-connected');
 
 function fetchData(user) {
-    axios.get(`https://api.github.com/users/${user}`)
-        .then(response => {
-            console.log("Username  : "+response.data.login+'\n'+
-                        (response.data.name ? ("Name      : "+response.data.name+'\n') : (""))+
-                        "Repos     : "+response.data.public_repos+'\n'+
-                        "Followers : "+response.data.followers+'\n'+
-                        "Following : "+response.data.following);
+    checkInternetConnected()
+        .then(result => {
+            axios.get(`https://api.github.com/users/${user}`)
+                .then(response => {
+                    console.log("Username  : "+response.data.login+'\n'+
+                                (response.data.name ? ("Name      : "+response.data.name+'\n') : (""))+
+                                "Repos     : "+response.data.public_repos+'\n'+
+                                "Followers : "+response.data.followers+'\n'+
+                                "Following : "+response.data.following);
+                })
+                .catch(err => {
+                    console.log(`${user} username doesn't exist.`);
+                })
         })
         .catch(err => {
-            console.log(`${user} username doesn't exist.`);
+            console.log("You are not connected to the internet");
         })
 }
 
